279c47671d9a135c1e6de2d29fc00f827adc0670
Derek Howard <Derek.howard@okstate.edu>
2024-03-19 15:27:44 -0500

    Initial Commit

---
+++ Ind02_Howard_Derek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ind02_Howard_Derek
+//
+//  Created by Laura Howard on 3/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
