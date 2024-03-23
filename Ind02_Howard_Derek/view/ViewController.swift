2a97de1b9ec87652774df44f255c9f03bbfda8e9
Derek Howard <Derek.howard@okstate.edu>
2024-03-22 23:58:37 -0500

    ran out of time

--- Ind02_Howard_Derek/view/ViewController.swift
+++ Ind02_Howard_Derek/view/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    /*
+    
+    var zeroY = "0"
+    var zeroX = "0"
     let blank = UIImage(named: "transparent square")
     
     @IBOutlet var rowOne: [UIImageView]!
@@ -33,20 +35,35 @@ class ViewController: UIViewController {
         ]
         let newBoard = SceneDelegate().shuffle()
         var count = 0
-        var x = 0
+        var x = -1
         var y = 0
         
         while count < 19 {
             if x > 2 {
-            board[y][x].image=UIImage(named: "\(newBoard[y][x]).png")
-        count+=1
                 x=0
                 y+=1
             } else {x+=1}
+            board[y][x].image=UIImage(named: "\(newBoard[y][x]).png")
+            if newBoard[y][x] == "0"{
+                zeroY=String(y)
+                zeroX=String(x)
+            }
+            count+=1
         }
-    }*/
+    }
     
     @IBAction func tapHandler(_ sender: UITapGestureRecognizer) {
+        var board: [[UIImageView]] = [
+            rowOne,rowTwo,rowThree,rowFour,rowFive
+        ]
+        let test = (Int(zeroY)!*4)+Int(zeroX)!
+        
+        if Int(sender.name!) == test-1 || Int(sender.name!) == test+1 || Int(sender.name!) == test+4 || Int(sender.name!) == test+4 {
+            
+            board[Int(zeroY)!][Int(zeroX)!].image=sender.view?.largeContentImage
+            sender.view?.largeContentImage=blank
+        }
+        
         print("Tap handled")
     }
     
